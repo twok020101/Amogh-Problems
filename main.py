fbfa51977cbbc01e0bb2e73f2eb4f7b3df441103
Kshitij Jayant Kurandwad <55920541+twok020101@users.noreply.github.com>
2022-06-07 23:37:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -16,33 +16,33 @@ def get_db():
     finally:
         db.close()
 
-app.post("/trees")
-def trees(val:schemas.TreeNew,db: Session=Depends(get_db)):
-    tree=Node(val.val,val.Name)
-    return tree
+@app.post("/trees")
+def trees_here(vals:schemas.TreeNew,db: Session=Depends(get_db)):
+    tree=Node(vals.val,vals.Name)
+    return tree.new_tree(db)
 
-app.post("/trees/{tree_id}/nodes")
+@app.post("/trees/{tree_id}/nodes")
 def nodes(tree_id:int,val:schemas.NodeNew,db: Session=Depends(get_db)):
     tree=crud.get_tree(db,tree_id)
     return trees.insert(tree,db,val.node_val,tree_id)
 
-app.delete("/trees/{tree_id}/nodes/{node_id}")
+@app.delete("/trees/{tree_id}/nodes/{node_id}")
 def delete(tree_id,node_id,db: Session=Depends(get_db)):
     val=crud.get_node_val(db,node_id,tree_id)
     tree=crud.get_tree(db,tree_id)
     return trees.delete(tree,val,tree_id,db)
 
-app.post("/trees/{tree_id}/inorder")
+@app.post("/trees/{tree_id}/inorder")
 def inorder(tree_id,db: Session=Depends(get_db)):
     tree=crud.get_tree(db,tree_id)
     return trees.inorder(tree)
 
-app.post("/trees/{tree_id}/preorder")
+@app.post("/trees/{tree_id}/preorder")
 def preorder(tree_id,db: Session=Depends(get_db)):
     tree=crud.get_tree(db,tree_id)
     return trees.preorder(tree)
 
-app.post("/trees/{tree_id}/postorder")
+@app.post("/trees/{tree_id}/postorder")
 def postorder(tree_id,db: Session=Depends(get_db)):
     tree=crud.get_tree(db,tree_id)
     return trees.postorder(tree)
